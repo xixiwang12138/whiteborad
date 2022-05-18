f3d5582ee7eccd0740e1fee4ff3dce595f9e336c
jyanon <804173948@qq.com>
2022-05-18 20:29:41 +0800

    [Enhancement] 提取组件

---
+++ components/avatar-display/avatar-display.ts
@@ -0,0 +1,15 @@
+
+Component({
+    properties: {
+        avatar: {
+            type: String
+        },
+        name: {
+            type: String
+        }
+    },
+    data: {
+    },
+    methods: {
+    }
+});
---
+++ components/gold-level-display/gold-level-display.ts
@@ -0,0 +1,21 @@
+
+Component({
+    properties: {
+        gold: {
+            type: Number,
+            value: 0
+        },
+        level: {
+            type: Number,
+            value: 1
+        },
+        expRate: {
+            type: Number,
+            value: 0
+        },
+    },
+    data: {
+    },
+    methods: {
+    }
+});

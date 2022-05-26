248a70c21fd84c0de5b5eb2a35977791ad2aa30a
Eddie Huang <1826942933@qq.com>
2022-05-27 00:35:06 +0800

    Login,Shop页面代码优化

--- pages/shop/ShopPage.ts
+++ pages/shop/ShopPage.ts
@@ -2,8 +2,35 @@ import {page, pageFunc} from "../common/PageBuilder";
 import {BasePage, BasePageData} from "../common/core/BasePage";
 import {playerMgr} from "../../modules/player/managers/PlayerManager";
 import {PlayerPage} from "../common/partPages/PlayerPage";
+import {field} from "../../modules/core/data/DataLoader";
 
-class Data extends BasePageData {}
+class Data extends BasePageData {
+
+	@field(Array)
+	Rooms= [{
+		roomStatus: 'changeable'
+	},{
+		roomStatus: 'using'
+	},{
+		roomStatus: 'purchasable',price: "4396"
+	},{
+		roomStatus: 'locked',price: "4396",unlock:"10级解锁购买"
+	},{
+		roomStatus: 'locked',price: "4396",unlock:"15级解锁购买"
+	},]
+	@field(Array)
+	Others= [{
+		roomStatus: 'purchasable',price: "4396"
+	},{
+		roomStatus: 'owned'
+	},{
+		roomStatus: 'owned'
+	},{
+		roomStatus: 'locked',price: "4396",unlock:"解锁房间购买"
+	},{
+		roomStatus: 'locked',price: "4396",unlock:"解锁房间购买"
+	},]
+}
 // const isRooms=true; 
 @page("shop", "商城")
 export class ShopPage extends BasePage<Data>{

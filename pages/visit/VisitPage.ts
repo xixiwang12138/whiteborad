01dd0164f648113658cf1d85454a6f654f66dd50
jyanon <804173948@qq.com>
2022-06-07 22:57:23 +0800

    [Enhancement] 修复弹窗尺寸问题

--- pages/visit/VisitPage.ts
+++ pages/visit/VisitPage.ts
@@ -1,25 +1,16 @@
 import { page, pageFunc } from "../common/PageBuilder";
 import {MainPage, MainPageData} from "../main/MainPage";
-import {BaseData} from "../../modules/core/data/BaseData";
 import {field} from "../../modules/core/data/DataLoader";
 import {roomMgr} from "../../modules/room/managers/RoomManager";
 import {Room} from "../../modules/room/data/Room";
-import {BasePageData} from "../common/core/BasePage";
-import {Focus, FocusTags, RuntimeFocus} from "../../modules/focus/data/Focus";
 
 class Params {
 	@field(String)
 	roomId: string
 }
 
-class Data extends MainPageData {
-
-}
-
 @page("visit", "他人房间")
 export class VisitPage extends MainPage<Params> {
-
-	public data = new Data();
 
 	async onLoad(e: Params) {
 		if (e.roomId) this.params.roomId ||= e.roomId;

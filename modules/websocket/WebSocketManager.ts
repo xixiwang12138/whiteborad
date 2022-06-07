b94e5011143dc88b638256d1e68e3b27f0528a6f
jyanon <804173948@qq.com>
2022-06-07 11:55:36 +0800

    [Enhancement] 修改服务器地址

--- modules/websocket/WebSocketManager.ts
+++ modules/websocket/WebSocketManager.ts
@@ -6,7 +6,8 @@ export function wsMgr() {
 	return getManager(WebSocketManager)
 }
 // const Host = "wss://homi-ws-server-1836805-1255510304.ap-shanghai.run.tcloudbase.com";
-const Host = "ws://localhost:3000";
+const Host = "wss://www.exermon.com:3000";
+// const Host = "ws://localhost:3000";
 
 @manager
 export class WebSocketManager extends BaseManager {

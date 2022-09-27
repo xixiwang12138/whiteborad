0ec919a6f18155a12d25867f964c2da92b877e74
jyanon <804173948@qq.com>
2022-09-27 15:25:24 +0800

    [Enhancement] 暴露ThemePage

--- pages/common/partPages/ThemePage.ts
+++ pages/common/partPages/ThemePage.ts
@@ -1,6 +1,6 @@
 import {PartialPage} from "../core/BasePage";
 import {BaseData} from "../../../modules/core/data/BaseData";
-import {Theme} from "../../../modules/room/data/Theme";
+import {Theme, themeRepo} from "../../../modules/room/data/Theme";
 import {DataOccasion, field, occasion} from "../../../modules/core/data/DataLoader";
 
 class Data extends BaseData {
@@ -12,7 +12,16 @@ export class ThemePage extends PartialPage<Data> {
 
   public data = new Data();
 
+  constructor() {
+    super();
+    console.log("ThemePage", this);
+  }
+
   public setTheme(theme: Theme) {
+    this.setData({theme});
+  }
+  public setThemeId(themeId: number) {
+    const theme = themeRepo().getById(themeId);
     this.setData({theme});
   }
 
